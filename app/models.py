ab1f140a13aac0e7158ca38452ea87dc0230e298
Vishal Vemula <vishaljvemula@gmail.com>
2025-10-19 17:54:25 +0530

    Added the User models in models.py

--- app/models.py
+++ app/models.py
@@ -1,9 +1,27 @@
 from typing import Annotated, Optional
+from pydantic import EmailStr
 from datetime import date
 from sqlmodel import Field, SQLModel
 
 PositiveAmount = Annotated[float, Field(ge= 0, description="Amount must be greater than zero", default=0)]
 DateCheck = Annotated[date, Field(default_factory=date.today)]
+
+class UserBase(SQLModel):
+    username: str
+    email: EmailStr | None = None
+    salary: int | None
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserRead(UserBase):
+    user_id: int | None 
+
+class User(UserBase, table=True):
+    user_id: int | None = Field(primary_key=True, default=None)
+    password_hash: str
+
 
 class ExpenseBase(SQLModel):
     name: str 
