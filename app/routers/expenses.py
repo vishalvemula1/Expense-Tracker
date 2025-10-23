9677f3d309b34095fa4447588c33fb28b0579060
Vishal Vemula <vishaljvemula@gmail.com>
2025-10-23 20:27:31 +0530

    Added verification for expenses so only users who are authorized can log in

--- app/routers/expenses.py
+++ app/routers/expenses.py
@@ -3,16 +3,17 @@ from fastapi import Query, APIRouter
 from sqlmodel import select
 from typing import Annotated
 from ..models import *
+from ..auth import VerifiedOwnerDep
 
 router = APIRouter(prefix="/users/{user_id}/expenses", tags=["expenses"])
 
 @router.get("/{expense_id}")
-async def read_an_expense(expense: ExpenseDep):
+async def read_an_expense(expense: ExpenseDep, verification: VerifiedOwnerDep):
     return expense
 
 @router.post("/")
-async def add_expense(user_id: int, expense: ExpenseCreate, user: UserDep, session: SessionDep):
-    expense_data = Expense.model_validate(expense, update={"user_id": user_id})
+async def add_expense(verified_user: VerifiedOwnerDep, expense: ExpenseCreate, session: SessionDep):
+    expense_data = Expense.model_validate(expense, update={"user_id": verified_user.user_id})
 
     session.add(expense_data)
     session.commit()
@@ -21,29 +22,28 @@ async def add_expense(user_id: int, expense: ExpenseCreate, user: UserDep, sessi
     return expense_data
 
 @router.get("/")
-async def read_expenses(user_id: int,
-                        user: UserDep,
+async def read_expenses(verified_user: VerifiedOwnerDep,
                         session: SessionDep, 
                         limit: Annotated[int, Query(le=100)] = 5,
                         offset: int = 0) -> list[Expense]:
     
     expenses = session.exec(
         select(Expense)
-        .where(Expense.user_id == user_id)
+        .where(Expense.user_id == verified_user.user_id)
         .limit(limit)
         .offset(offset)
         ).all()
     return list(expenses)
 
 @router.delete("/{expense_id}")
-async def delete_expense(expense: ExpenseDep, session: SessionDep):
+async def delete_expense(expense: ExpenseDep, verification: VerifiedOwnerDep, session: SessionDep):
     session.delete(expense)
     session.commit()
 
     return expense
 
 @router.put("/{expense_id}")
-async def update_expense(expense: ExpenseDep, update_request: ExpenseUpdate, session: SessionDep):
+async def update_expense(expense: ExpenseDep, update_request: ExpenseUpdate, verification: VerifiedOwnerDep, session: SessionDep):
     update_data = update_request.model_dump(exclude_unset=True)
 
     expense.sqlmodel_update(update_data)
