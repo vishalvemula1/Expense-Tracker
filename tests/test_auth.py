df05e76eaa92ec2ceeaabffaadbbc14909030b23
Vishal Vemula <vishaljvemula@gmail.com>
2025-10-26 17:37:25 +0530

    Added unit  tests for get_authenticated_user function from auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,14 +1,14 @@
-from fastapi.testclient import TestClient
-from app.auth import authenticate_user, verify_password, create_token 
+from app.auth import authenticate_user, verify_password, create_token, get_authenticated_user 
 from fastapi import HTTPException
 from pytest import raises
 from app.models import User
-from app.config import settings
 import jwt
+from sqlmodel import Session
+import pytest
 
-# ========================================
+# ====================================================================
 # Testing for authenticate_user from app/auth.py 
-# ========================================
+# ====================================================================
 def test_authenticate_happy_path(test_db, test_user):
     result = authenticate_user("testuser", "testpassword", test_db)
     assert result.username == "testuser"
@@ -18,16 +18,19 @@ def test_authenticate_user_wrong_password(test_db, test_user):
         authenticate_user("testuser", "wrongpassword", test_db)
 
     assert exc_info.value.status_code == 401
-    
+    assert exc_info.value.detail == "Invalid username or password"
+
 def test_authenticate_user_not_found(test_db, test_user):
     with raises(HTTPException) as exc_info:
         authenticate_user("nonexistantuser", "testpassword", test_db)
     
     assert exc_info.value.status_code == 401
+    assert exc_info.value.detail == "Invalid username or password"
 
-# ========================================
+
+# ====================================================================
 # Testing for verify_password from app/auth.py
-# ========================================
+# ====================================================================
 
 def test_verify_password_happy_path(test_user: User):
     result = verify_password("testpassword", test_user.password_hash)
@@ -37,11 +40,12 @@ def test_verify_password_wrong_password(test_user: User):
     result = verify_password("wrongpassword", test_user.password_hash)  
     assert result is False
 
-# ========================================
+# ====================================================================
 # Testing for create_token from app/auth.py
-# ========================================
+# ====================================================================
 
 def test_create_token():
+    from app.config import settings
     from typing import Any
 
     user_id = 1
@@ -54,6 +58,7 @@ def test_create_token():
 
 def test_create_token_with_expiration():
     from datetime import timedelta, timezone, datetime
+    from app.config import settings
     from typing import Any
 
     user_id = 1
@@ -70,3 +75,55 @@ def test_create_token_with_expiration():
     expected_max = after + (time * 60)
 
     assert expected_min <= payload["exp"] <= expected_max
+
+# ====================================================================
+# Testing for get_authenticated_user from app/auth.py
+# ====================================================================
+
+@pytest.mark.asyncio
+async def test_get_authenticated_user_happy_path(test_db: Session, test_user: User):
+    user_id = test_user.user_id
+    assert user_id is not None
+
+    token = create_token(user_id)
+
+    result = await get_authenticated_user(token, test_db)
+    assert result == test_user
+
+@pytest.mark.asyncio
+async def test_get_authenticated_user_wrong_user(test_db: Session, test_user: User):
+
+    token = "stringforme"
+
+    with raises(HTTPException) as exc_info:
+        await get_authenticated_user(token, test_db)
+
+    assert exc_info.value.status_code == 401
+    assert exc_info.value.detail == "Could not validate credentials"
+
+@pytest.mark.asyncio
+async def test_get_authenticated_user_expired_token(test_db: Session, test_user: User):
+    from datetime import timedelta
+
+    user_id = test_user.user_id
+    assert user_id is not None
+
+    token = create_token(user_id, expires_delta=timedelta(seconds=-1))
+
+    with raises(HTTPException) as exc_info:
+        await get_authenticated_user(token, test_db)
+    
+    assert exc_info.value.status_code == 401
+    assert exc_info.value.detail == "Could not validate credentials"
+    
+@pytest.mark.asyncio
+async def test_get_authenticated_user_no_user(test_db: Session, test_user: User):
+    user_id = 3
+
+    token = create_token(user_id)
+
+    with raises(HTTPException) as exc_info:
+        await get_authenticated_user(token, test_db)
+    
+    assert exc_info.value.status_code == 404
+    assert exc_info.value.detail == "User was not found"
