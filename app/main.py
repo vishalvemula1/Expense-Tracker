36da219c1e2846e3d002bc4d0b2285193412f9b1
Vishal Vemula <vishaljvemula@gmail.com>
2025-10-26 18:49:17 +0530

    Changed the on_event logic which is deprecated and moved to the asynccontextmanager using lifespan

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from .database import create_db_and_tables
 from .routers.expenses import router as expenses_router
 from .routers.users import router as users_router
 
-app = FastAPI()
 
-@app.on_event("startup")
-def on_startup() -> None:
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(expenses_router)
 
