7bb3591ecba1039d8d9e362045c3070c7fa5fd3b
Vishal Vemula <vishaljvemula@gmail.com>
2025-10-27 12:00:16 +0530

    Added tests for add_expense (post) endpoint

--- tests/test_expenses.py
+++ tests/test_expenses.py
@@ -44,3 +44,63 @@ def test_read_expense_unauthenticated(test_expense: Expense, test_user: User, cl
     assert response.status_code == 401
     data = response.json()
     assert data["detail"] == "Not authenticated"
+
+# ====================================================================
+# Testing for the add expense (post) endpoint in expenses.py
+# ====================================================================
+
+def test_add_expense_happy_path(authenticated_client: TestClient, test_user: User, test_expense):
+    response = authenticated_client.post(
+        f"/users/{test_user.user_id}/expenses/", json={
+            "name": "Cheese",
+            "amount": 50,
+            "category": "Food",
+            "description": "Unforunate lack of judgement"
+        }
+        )
+    
+    assert response.status_code == 200
+    data = response.json()
+    assert data["expense_id"] == 2
+    assert data["user_id"] == 1
+
+def test_add_expense_not_authenticated(client: TestClient, test_user: User, test_expense: Expense):
+    response = client.post(
+        f"/users/{test_user.user_id}/expenses/", json={
+            "name": "Cheese",
+            "amount": 50,
+            "category": "Food",
+            "description": "Unforunate lack of judgement"
+        }
+        )
+    
+    assert response.status_code == 401
+    data = response.json()
+    assert data["detail"] == "Not authenticated"
+
+def test_add_expense_unauthorized(authenticated_client: TestClient, test_user: User, test_expense: Expense):
+    response = authenticated_client.post(
+        f"/users/{3}/expenses/", json={
+            "name": "Cheese",
+            "amount": 50,
+            "category": "Food",
+            "description": "Unforunate lack of judgement"
+        }
+        )
+    
+    assert response.status_code == 403
+    data = response.json()
+    assert data["detail"] == "Not authorized for this request"
+
+def test_add_expense_invalid_data(authenticated_client: TestClient, test_user: User, test_expense: Expense):
+    response = authenticated_client.post(
+        f"/users/{test_user.user_id}/expenses/", json={
+            "name": "",
+            "amount": -50,
+            "category": "Food",
+            "description": "Unforunate lack of judgement"
+        }
+        )
+    
+    assert response.status_code == 422
+
