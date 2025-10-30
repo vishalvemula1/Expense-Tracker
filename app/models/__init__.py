9b72ba2b83c55fb29b574df28e6915717bc7844b
Vishal Vemula <vishaljvemula@gmail.com>
2025-10-30 10:58:09 +0530

    Updated init to include category models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,10 @@
 from .user import UserBase, UserCreate, UserRead, User, UserUpdate
 from .expense import ExpenseBase, ExpenseCreate, ExpenseUpdate, Expense
 from .token import Token
+from .category import Category, CategoryBase, CategoryCreate, CategoryUpdate
 
 __all__ = [
     "UserBase", "UserCreate", "UserRead", "User", "UserUpdate",
     "ExpenseBase", "ExpenseCreate", "ExpenseUpdate", "Expense",
-    "Token"
+    "Token", "Category", "CategoryBase", "CategoryCreate", "CategoryUpdate"
 ]
