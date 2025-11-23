a02d96cf25acfff65382a97721b52862a0078ef6
Vishal Vemula <vishaljvemula@gmail.com>
2025-11-23 11:16:13 +0530

    Fixed auth_service to include login

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -14,11 +14,24 @@ class AuthService:
 
 
     def create_user_with_defaults(self, user: UserCreate) -> User:
+        """This also creates a default category from event listeners (check app/models/event.py)"""
+
         with db_transaction(self.session, context="User Signup") as db:
             hashed_password = get_password_hash(user.password)
+            user_data_dict = user.model_dump(exclude={"password"})
+            new_user = User.model_validate(user_data_dict, update={"password_hash": hashed_password})
+
+            db.add(new_user)
+            db.commit()
+
+            db.refresh(new_user)
+
+        return new_user
+
+    def login(self, form_data: OAuth2PasswordRequestForm) -> Token:
         user = authenticate_user(form_data.username, form_data.password, self.session)
 
-        token = create_token(user.user_id) #type: ignore
+        token = create_token(user.user_id)
 
         return Token(access_token=token, token_type="bearer")
     
